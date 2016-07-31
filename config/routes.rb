8a9ac8963f8994fdb19183084028337a2f299c09
nafiskarim <sahil.abrar@gmail.com>
2016-08-01 01:52:31 +0200

    friend search and add func added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, ony: [:show]
+  resources :friendships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'  
 
@@ -10,4 +12,9 @@ Rails.application.routes.draw do
   get 'search_stocks', to: "stocks#search"
 
   get 'my_friends', to: "users#my_friends"
+
+  get 'search_friends', to: "users#search"
+  
+  post 'add_friend', to: "users#add_friend"
 end
+
